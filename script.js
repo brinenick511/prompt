83d5f30b0df2ecfebe487b82f8c746c293e3edaa
brinenick511-venus <brinenick511@gmail.com>
2024-07-17 20:24:52 +0800

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function copyToClipboard(id) {
     var textarea = document.getElementById(id);
     textarea.select();
     document.execCommand('copy');
-    alert('Copied to clipboard!');
+    // alert('Copied to clipboard!');
 }
